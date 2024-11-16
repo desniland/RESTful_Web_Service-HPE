eaa1476bef87b79c022b68258ac4fde2dd052e8b
desniland <albertdesmond26@gmail.com>
2024-11-16 10:10:20 +0300

    simple java application

---
+++ start/src/main/java/com/example/hpedemo/demo-application.java
@@ -0,0 +1,13 @@
+package com.example.hpedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(demo_application.class, args);
+    }
+
+}
